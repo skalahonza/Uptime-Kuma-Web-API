7d3605f764b53940ff4016fe02b1b1df6ce5fd8b
Jan Skála <skalahonza@users.noreply.github.com>
2024-11-27 11:15:15 +0100

    Register the `get_api_key` dependency globally on the router
    * Remove individual `Depends(get_api_key)` dependencies from each router registration

--- app/main.py
+++ app/main.py
@@ -20,21 +20,21 @@ from config import settings as app_settings, logger
 from app_setup import initialize_app
 from utils.deps import get_api_key
 
-app = FastAPI(title=app_settings.PROJECT_NAME)
+app = FastAPI(title=app_settings.PROJECT_NAME, dependencies=[Depends(get_api_key)])
 app.router.redirect_slashes = True
 
-app.include_router(user.router, prefix="/users", tags=["Users"], dependencies=[Depends(get_api_key)])
-app.include_router(settings.router, prefix="/settings", tags=["Settings"], dependencies=[Depends(get_api_key)])
-app.include_router(database.router, prefix="/database", tags=["DataBase"], dependencies=[Depends(get_api_key)])
-app.include_router(monitor.router, prefix="/monitors", tags=["Monitor"], dependencies=[Depends(get_api_key)])
-app.include_router(statuspage.router, prefix="/statuspages", tags=["Status Page"], dependencies=[Depends(get_api_key)])
-app.include_router(maintenance.router, prefix="/maintenance", tags=["Maintenance"], dependencies=[Depends(get_api_key)])
-app.include_router(tags.router, prefix="/tags", tags=["Tags"], dependencies=[Depends(get_api_key)])
-app.include_router(cert.router, prefix="/cert_info", tags=["Certification Info"], dependencies=[Depends(get_api_key)])
-app.include_router(info.router, prefix="/info", tags=["Informations"], dependencies=[Depends(get_api_key)])
-app.include_router(ping.router, prefix="/ping", tags=["Ping Average"], dependencies=[Depends(get_api_key)])
-app.include_router(uptime.router, prefix="/uptime", tags=["Uptime"], dependencies=[Depends(get_api_key)])
-app.include_router(auth.router, prefix="/login", tags=["Authentication"], dependencies=[Depends(get_api_key)])
+app.include_router(user.router, prefix="/users", tags=["Users"])
+app.include_router(settings.router, prefix="/settings", tags=["Settings"])
+app.include_router(database.router, prefix="/database", tags=["DataBase"])
+app.include_router(monitor.router, prefix="/monitors", tags=["Monitor"])
+app.include_router(statuspage.router, prefix="/statuspages", tags=["Status Page"])
+app.include_router(maintenance.router, prefix="/maintenance", tags=["Maintenance"])
+app.include_router(tags.router, prefix="/tags", tags=["Tags"])
+app.include_router(cert.router, prefix="/cert_info", tags=["Certification Info"])
+app.include_router(info.router, prefix="/info", tags=["Informations"])
+app.include_router(ping.router, prefix="/ping", tags=["Ping Average"])
+app.include_router(uptime.router, prefix="/uptime", tags=["Uptime"])
+app.include_router(auth.router, prefix="/login", tags=["Authentication"])
 
 
 @app.on_event("startup")
